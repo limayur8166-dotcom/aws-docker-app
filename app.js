1c7326daab80087053d43d3b7a7ef417d8b2b71c
limayur8166-dotcom <limayur8166@gmail.com>
2026-03-26 17:54:12 +0530

    Update greeting message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from AWS ECS Docker!");
+  res.send("🚀 Hello from AWS ECS Docker  new chnage !");
 });
 
 app.listen(3000, () => {
